e0f5b5c4f659c1e8021b431bdfbfbd32acc58efb
JohnnyPP <johnnnyppp@gmail.com>
2016-02-09 21:04:26 +0100

    Initial release

---
+++ JobApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace JobApplication
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// in initalization load the previous values from the variables folder
+			// modify
+			// press button and update variables
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
